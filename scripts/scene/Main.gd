eee06dcd8d3c9e6143272699c8b8faa81a495392
Nik Mirza <nik96mirza@gmail.com>
2019-06-08 11:46:56 +0800

    0.0.1 proof of concept

--- scripts/scene/Main.gd
+++ scripts/scene/Main.gd
@@ -13,7 +13,7 @@ func _ready():
 	VisualServer.set_default_clear_color(clear_color);
 	SceneManager.nextLevel(defaultLevel);
 
-	hub.connect("channel",self,"_on_connected");
+	hub.connect("channel",self,"_on_channel");
 	hub.conn.connect("connected",self,"_on_connected");
 	hub.conn.connect("join",self,"_on_join");
 	hub.conn.connect("left",self,"_on_left");
